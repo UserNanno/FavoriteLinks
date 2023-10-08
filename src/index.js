cf03208d244cdecdbebdcd8d14ef4d0748c84200
UserNanno <juan.canecillas@unmsm.edu.pe>
2023-10-08 15:01:59 -0500

    Configurando las rutas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 //  initializations
 const app = express();
 
 //  settings
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
 
 // Midedlewares
 app.use(morgan('dev'));
